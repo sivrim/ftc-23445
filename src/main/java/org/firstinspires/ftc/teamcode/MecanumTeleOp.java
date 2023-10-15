7117af0da7012cfab40922633458cd1825740f6e
sivrim <64481915+sivrim@users.noreply.github.com>
2023-10-15 17:57:05 -0400

    assign gamepad2 left stick y

--- src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -63,6 +63,8 @@ public class MecanumTeleOp extends LinearOpMode {
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
+            double armY = gamepad2.left_stick_y;
+
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
