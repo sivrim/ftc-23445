6fd8554b75cf544086cdcd796002bf061c35f6d1
sivrim <64481915+sivrim@users.noreply.github.com>
2023-10-15 18:14:52 -0400

    rename variables

--- src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -59,20 +59,20 @@ public class MecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            double chassisY = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double chassisX = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double chassisTurn = gamepad1.right_stick_x;
 
             double armY = gamepad2.left_stick_y;
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(chassisY) + Math.abs(chassisX) + Math.abs(chassisTurn), 1);
+            double frontLeftPower = (chassisY + chassisX + chassisTurn) / denominator;
+            double backLeftPower = (chassisY - chassisX + chassisTurn) / denominator;
+            double frontRightPower = (chassisY - chassisX - chassisTurn) / denominator;
+            double backRightPower = (chassisY + chassisX - chassisTurn) / denominator;
 
             frontLeftMotor.setPower(frontLeftPower/2);
             backLeftMotor.setPower(backLeftPower/2);
