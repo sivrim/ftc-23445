f6778d34278ae4f6727e32ad7bb175f13060b101
The-Asterisk <148398718+The-Asterisk@users.noreply.github.com>
2023-11-07 12:13:35 -0800

    Update OpModeAuto.java
    FINALLY! Quadrant Detection with ZERO SYNTAX ERRORS!!!

--- src/main/java/org/firstinspires/ftc/teamcode/OpModeAuto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/OpModeAuto.java
@@ -5,23 +5,27 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous(name="Auto", group="Furious Frog")
+@Autonomous(name="Auto", group="Furious Frogs")
 @Disabled
-public class OpModeAuto extends LinearOpMode
-{
+public class OpModeAuto extends LinearOpMode {
 
     /* Declare OpMode members. */
     DcMotor armMotor = null;
     private ElapsedTime runtime = new ElapsedTime();
 
-    @Override
+        @Override
     public void runOpMode() {
 
         MacanumWheels wheels = new MacanumWheels(hardwareMap);
 
+        DistanceSensor leftDistanceSensor = (DistanceSensor) hardwareMap.get("left2m");
+        DistanceSensor rightDistanceSensor = (DistanceSensor) hardwareMap.get("right2m");
+
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
+        telemetry.addData("Status", "Ready to run");
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -30,7 +34,7 @@ public class OpModeAuto extends LinearOpMode
         // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
         //TODO -- Come up with these steps. -- Ani2 and Adi
 
-        // Step 1:  Drive forward
+        // Step 1:  Drive forward 12 inches
         // TODO --  Determine how long we will need to go, in terms of distance and/or time -- Ani1 and Vishnu
         wheels.goForward();
 
@@ -43,12 +47,45 @@ public class OpModeAuto extends LinearOpMode
         // Step 2:  Turn and keep on looking for April tags
         //TODO
         runtime.reset();
+
         while (opModeIsActive() /* && Apritags not detected */) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
 
-        //Step 2.5 TODO based on april tag reading, determine quadrant Kermit was placed in
+        //Step 2.5 TODO -- Determine quadrant Kermit was placed in
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.4)) {
+            //leftDistance rightDistance
+            double l = leftDistanceSensor.getDistance(DistanceUnit.INCH);
+            double r = rightDistanceSensor.getDistance(DistanceUnit.INCH);
+
+
+            if ((inRange(l, 48, 60) && (inRange(r, 24, 36) || inRange(r, 72, 84)))) /* A4 */ {
+                // strat here
+            }
+
+            else if ((inRange(l, 24, 36) || inRange(l, 72, 84)) && inRange(r, 48, 60)) /* F4 */ {
+                // strat here
+            }
+
+            else if (inRange(l, 24, 36) && (inRange(r, 24, 36) || inRange(r, 96, 108))) /* F2 */ {
+                // strat here
+            }
+
+            else if ((inRange(l, 24, 36) || inRange(l, 96, 108)) && inRange(r, 24, 36)) /* A2 */ {
+                // strat here
+            }
+                    /*                     * && r = 36-44 INCHES and right w/ robot = 24-32 INCHES) {
+                quadrant = 1 // A4 }
+            else if left = 24-32 INCHES and (right = 24-32 INCHES and right w/ robot = 24-32 INCHES) {
+                quadrant = 2 // F4 }
+            else if left = 12-20 INCHES and (right = 48-56 INCHES and right w/ robot = 24-32 INCHES) {
+                quadrant = 3 // F2 }
+            else if left = 24-32 INCHES and (right = 12-20 INCHES and right w/ robot = 12-20 INCHES) {
+                quadrant = 4 // A2 }
+            */
+        }
 
         // Step 3: Rotate back to original orientation
         //TODO
@@ -86,4 +123,7 @@ public class OpModeAuto extends LinearOpMode
         sleep(1000);
     }
 
+public boolean inRange(double distance, int start, int end){
+    return distance > start && distance < end;
+    }
 }
